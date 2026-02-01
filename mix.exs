94664e01e6cfaa319760e4740568f0151b48032b
Sean Callan <doomspork@users.noreply.github.com>
2026-02-01 10:48:41 -0500

    ci: add pr validation and improve ci workflows

--- mix.exs
+++ mix.exs
@@ -10,7 +10,9 @@ defmodule Ricqchet.MixProject do
       version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
       deps: deps(),
+      dialyzer: dialyzer(),
       description: description(),
       package: package(),
       docs: docs(),
@@ -30,10 +32,33 @@ defmodule Ricqchet.MixProject do
       {:req, "~> 0.5"},
       {:jason, "~> 1.4"},
       {:plug, "~> 1.14", optional: true},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+
+      # Dev/Test tooling
       {:bypass, "~> 2.1", only: :test},
-      {:mox, "~> 1.0", only: :test}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:mox, "~> 1.1", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --check-unused",
+        "format",
+        "credo --strict",
+        "dialyzer",
+        "test"
+      ]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_add_apps: [:ex_unit],
+      plt_file_path: "priv/plts"
     ]
   end
 
